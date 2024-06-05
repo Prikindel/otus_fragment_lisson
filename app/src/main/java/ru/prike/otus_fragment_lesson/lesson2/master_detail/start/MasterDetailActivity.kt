4c0aa590a3a5e30d9dcf8611f6b9b74bf744b8c6
KiryakovMaxim <kiryakovm96@gmail.com>
2024-06-05 20:58:10 +0300

    finish maser detail lesson 2

--- app/src/main/java/ru/prike/otus_fragment_lesson/lesson2/master_detail/start/MasterDetailActivity.kt
+++ app/src/main/java/ru/prike/otus_fragment_lesson/lesson2/master_detail/start/MasterDetailActivity.kt
@@ -1,9 +1,12 @@
 package ru.prike.otus_fragment_lesson.lesson2.master_detail.start
 
 import android.os.Bundle
+import android.util.Log
+import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.replace
 import ru.prike.otus_fragment_lesson.R
 import ru.prike.otus_fragment_lesson.lesson2.master_detail.Item
 
@@ -18,9 +21,24 @@ class MasterDetailActivity : FragmentActivity(), MasterDetailListFragment.OnItem
     }
 
     private fun determinePaneLayout() {
+        val fragmentDetailContainer = findViewById<ViewGroup>(R.id.flDetailContainer)
+        if (fragmentDetailContainer != null) {
+            isTablet = true
+        }
     }
 
     override fun onItemSelected(item: Item?) {
-
+        val fragmentDetail = MasterDetailFragment.newInstance(item)
+        if (isTablet) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.flDetailContainer, fragmentDetail)
+                .addToBackStack("list_to_detail")
+                .commit()
+        } else {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment1, fragmentDetail)
+                .addToBackStack("list_to_detail")
+                .commit()
+        }
     }
 }
