8a94de636f2896aede6e66028b305f0f5d96cca0
KiryakovMaxim <kiryakovm96@gmail.com>
2025-11-10 21:02:48 +0300

    Added Master Detail

--- app/src/main/java/ru/prike/otus_fragment_lesson/lesson2/master_detail/start/MasterDetailActivity.kt
+++ app/src/main/java/ru/prike/otus_fragment_lesson/lesson2/master_detail/start/MasterDetailActivity.kt
@@ -1,6 +1,7 @@
 package ru.prike.otus_fragment_lesson.lesson2.master_detail.start
 
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.FragmentTransaction
@@ -18,9 +19,35 @@ class MasterDetailActivity : FragmentActivity(), MasterDetailListFragment.OnItem
     }
 
     private fun determinePaneLayout() {
+//        val fragmentDetailContainer = findViewById<ViewGroup>(R.id.flDetailContainer)
+//        if (fragmentDetailContainer != null) {
+//            isTablet = true
+//        }
+        isTablet = resources.getBoolean(R.bool.is_tablet)
+        println("isTablet = $isTablet")
     }
 
     override fun onItemSelected(item: Item?) {
-
+        if (isTablet) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.flDetailContainer, MasterDetailFragment.newInstance(item))
+//                .addToBackStack("")
+                .commit()
+        } else {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment1, MasterDetailFragment.newInstance(item))
+                .addToBackStack("")
+                .commit()
+        }
     }
+
+    // Если в layout контейнеры в разметке для телефона и в разметке для планшета имеют
+// одинаковый id, то можем делать вот так:
+
+//    override fun onItemSelected(item: Item?) {
+//        supportFragmentManager.beginTransaction()
+//            .replace(R.id.fragment1, MasterDetailFragment.newInstance(item))
+//            .addToBackStack("")
+//            .commit()
+//    }
 }
